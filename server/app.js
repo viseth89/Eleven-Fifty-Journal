b42c2ed7616fa133dc09784ec8ae2b21c522a599
viseth89 <viseth89@gmail.com>
2021-06-26 14:11:47 -0500

    Worked through 4.2

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 const Express = require("express")
 const app = Express();
 
+const controllers = require('./controllers');
+
+
+
+
+app.use("/journal", controllers.journalController)
+
 app.listen(3000, () => {
     console.log(`[Server]: App is listening on 3000.`)
 })
--- server/controllers/journalcontroller.js
+++ server/controllers/journalcontroller.js
@@ -1,8 +1,8 @@
 const Express = require('express');
 const router = Express.Router();
 
-router.get('/practice', (req,res)=> {
-    res.send('Hey this is a practice route!')
-})
+router.get('/practice', (req, res) => {
+    res.send('Hey')
+});
 
 module.exports = router
