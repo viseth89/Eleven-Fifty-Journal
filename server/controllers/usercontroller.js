6b78620996ec880654dec3b1f9760e03361e8ad3
viseth89 <viseth89@gmail.com>
2021-06-27 08:21:00 -0500

    Returning a response in 9.2.4 -> 9.2.3 and 9.2.2 make a bit more sense now

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const { UserModel } = require('../models');
 
-router.post("/register",  (req, res) => {
+router.post("/register",  async (req, res) => {
 
     let {email, password } = req.body.user;
-    UserModel.create({
+    await UserModel.create({
         email,
         password
     })
+
+    res.send("This is our user/register endpoint!")
 })
 
 module.exports =router;
