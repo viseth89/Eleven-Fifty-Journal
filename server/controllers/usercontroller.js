88c2ee875716c9d629ade16ffa2557347bea0c2e
viseth89 <viseth89@gmail.com>
2021-06-27 10:28:41 -0500

    Added try/catch from 9.2.6 for error handling

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -1,18 +1,31 @@
-const router = require('express').Router();
-const { UserModel } = require('../models');
+const router = require("express").Router();
+const { UserModel } = require("../models");
+const { UniqueConstraintError } = require("sequelize/lib/errors")
 
-router.post("/register",  async (req, res) => {
 
-    let {email, password } = req.body.user;
+router.post("/register", async (req, res) => {
+  let { email, password } = req.body.user;
+  try {
     const User = await UserModel.create({
-        email,
-        password
-    })
+      email,
+      password,
+    });
 
     res.status(201).json({
-        message: "User successfully registered",
-        user: User
-    })
-})
+      message: "User successfully registered",
+      user: User,
+    });
+  } catch (err) {
+    if (err instanceof UniqueConstraintError) {
+      res.status(409).json({
+        message: "Email already in use",
+      });
+    } else {
+      res.status(500).json({
+        message: "Failed to register user",
+      });
+    }
+  }
+});
 
-module.exports =router;
+module.exports = router;
