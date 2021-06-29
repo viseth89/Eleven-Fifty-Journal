cbed2df54227a8c0c15b407a9d23c5e952e3fd0d
viseth89 <viseth89@gmail.com>
2021-06-29 06:21:46 -0500

    Debugging 12.3.4

--- server/controllers/journalcontroller.js
+++ server/controllers/journalcontroller.js
@@ -32,8 +32,47 @@ router.post("/create", validateJWT, async(req, res) => {
 
 })
 
-router.get('/about', (req,res) => {
-    res.send('This is about')
+router.get('/', async (req,res) => {
+    try{
+        const entries = await JournalModel.findAll();
+        res.status(200).json(entries);
+    } catch (err) {
+        res.status(500).json({error:err})
+    }
 })
 
+/* 
+Get Journals by User
+*/
+
+router.get("/mine", validateJWT, async(req, res) => {
+    const {id} = req.user;
+    try {
+        const userJournals = await JournalModel.findAll({
+            where: {
+                owner: id
+            }
+        });
+        res.status(200).json(userJournals);
+    } catch (err) {
+        res.status(500).json({error: err})
+    }
+})
+
+
+/* 
+Get Journals by Title
+ */
+
+router.get("/ttittle", async (req, res) => {
+    const {title} = req.params;
+    try {
+        const results = await JournalModel.findAll({
+            where: { title: title}
+        });
+        res.status(200).json(results);
+    } catch (err) {
+        res.status(500).json({ error: err})
+    }
+})
 module.exports = router
