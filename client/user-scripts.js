0e9d16345d28c0ce363176d54e62ad17f4d6effb
viseth89 <viseth89@gmail.com>
2021-06-28 21:04:22 -0500

    working through 1.2 script setup

---
+++ client/user-scripts.js
@@ -0,0 +1,31 @@
+/* *************************
+*** USER SIGNUP ***
+************************** */
+function userSignUp() {
+     console.log('userSignUp Function Called')
+    }
+    
+    
+    /* *************************
+    *** USER LOGIN ***
+    ************************** */
+    function userLogin() {
+     console.log('userLogin Function Called')
+    }
+    
+    
+    /* *************************
+    *** USER LOGOUT ***
+    ************************** */
+    function userLogout() {
+     console.log('userLogout Function Called')
+    }
+    
+    
+    /* *************************
+     *** TOKEN CHECKER FUNCTION ***
+    ************************** */
+    function tokenChecker() {
+     console.log('tokenChecker Function Called')
+    }
+    tokenChecker()
