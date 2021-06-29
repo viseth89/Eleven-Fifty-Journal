4bb0238a732d21131da0c0ec14553416e79907d4
viseth89 <viseth89@gmail.com>
2021-06-29 15:03:46 -0500

    Finished 4.5 debugging issues

--- client/get-scripts.js
+++ client/get-scripts.js
@@ -80,16 +80,152 @@ function displayMine() {
     });
 }
 
+
 /* *************************
  *** DISPLAY ALL ***
  ************************** */
 function displayAll() {
   console.log("displayAll Function Called");
+
+  fetch(`http://localhost:3000/journal/`, {
+    method: "GET",
+    headers: new Headers({
+      "Content-Type": "application/json",
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+
+    let display = document.getElementById('journals');
+    for(i=0; i=display.childNodes.length; i++) {
+      display.removeChild(display.firstChild)
+    }
+    // Cleanup
+    if (data.length === 0) {
+      let display = document.getElementById("journals");
+      let header = document.createElement("h5");
+
+      display.appendChild(header);
+      header.textContent = "You haven't made any posts yet!";
+      header.setAttribute("class", "noPosts");
+    } else {
+      for (i = 0; i < data.length; i++) {
+        let display = document.getElementById("journals");
+        let card = document.createElement("div");
+        let body = document.createElement("div");
+        let header = document.createElement("h5");
+        let subtitle = document.createElement("h6");
+        let para = document.createElement("p");
+
+
+        let current = data[i];
+        let title = current.title;
+        let date = current.date;
+        let entry = current.entry;
+
+        header.textContent = title;
+        subtitle.textContent = date;
+        para.textContent=entry;
+
+
+        display.appendChild(card);
+        card.appendChild(body);
+        body.appendChild(header);
+        body.appendChild(subtitle);
+        body.appendChild(para);
+        body.appendChild(editBtn);
+        body.appendChild(deleteBtn);
+
+        card.setAttribute('id', current.id);
+        card.setAttribute('class', 'card');
+        body.setAttribute('class', 'card-body');
+        header.setAttribute('class', 'card-title');
+        subtitle.setAttribute('class', 'card-subtitle mb-2 text-muted');
+        para.setAttribute('class', 'card-text')
+
+
+      }
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 }
+
+
 
 /* *************************
  *** DISPLAY BY TITLE ***
  ************************** */
 function displayByTitle() {
   console.log("displayByTitle Function Called");
+
+  let journalTitle = document.getElementById('searchBar').value;
+  console.log(journalTitle);
+
+  fetch(`http://localhost:3000/journal/${journalTitle}`, {
+    method: "GET",
+    headers: new Headers({
+      "Content-Type": "application/json",
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+
+    let display = document.getElementById('journals');
+    for (i=0; i=display.childNodes.length; i++) {
+      display.removeChild(display.firstChild)
+    }
+
+    if (data.length === 0) {
+      let display = document.getElementById("journals");
+      let header = document.createElement("h5");
+
+      display.appendChild(header);
+      header.textContent = "You haven't made any posts yet!";
+      header.setAttribute("class", "noPosts");
+    } else {
+      for (i = 0; i < data.length; i++) {
+        let display = document.getElementById("journals");
+        let card = document.createElement("div");
+        let body = document.createElement("div");
+        let header = document.createElement("h5");
+        let subtitle = document.createElement("h6");
+        let para = document.createElement("p");
+
+
+        let current = data[i];
+        let title = current.title;
+        let date = current.date;
+        let entry = current.entry;
+
+        header.textContent = title;
+        subtitle.textContent = date;
+        para.textContent=entry;
+
+
+        display.appendChild(card);
+        card.appendChild(body);
+        body.appendChild(header);
+        body.appendChild(subtitle);
+        body.appendChild(para);
+        body.appendChild(editBtn);
+        body.appendChild(deleteBtn);
+
+        card.setAttribute('id', current.id);
+        card.setAttribute('class', 'card');
+        body.setAttribute('class', 'card-body');
+        header.setAttribute('class', 'card-title');
+        subtitle.setAttribute('class', 'card-subtitle mb-2 text-muted');
+        para.setAttribute('class', 'card-text')
+
+
+      }
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 }
--- client/post-edit-delete-scripts.js
+++ client/post-edit-delete-scripts.js
@@ -1,68 +1,62 @@
 /* *************************
  *** POST JOURNAL ***
-************************** */
+ ************************** */
 function postJournal() {
+  console.log("postJournal Function Called");
+  const accessToken = localStorage.getItem("sessionToken");
+  let title = document.getElementById("title").value;
+  let date = document.getElementById("date").value;
+  let entry = document.getElementById("entry").value;
 
-    
-     console.log('postJournal Function Called')
-    const accessToken = localStorage.getItem('sessionToken')
-    let title = document.getElementById('title').value;
-    let date = document.getElementById('date').value;
-    let entry = document.getElementById('entry').value
-    
-    let newEntry = {
-        journal: {
-            title: title,
-            date: date,
-            entry: entry
-        }
-    }
+  let newEntry = {
+    journal: {
+      title: title,
+      date: date,
+      entry: entry,
+    },
+  };
 
-    fetch(`http://localhost:3000/journal/create`, {
-        method: "POST",
-        headers: new Headers({
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${accessToken}`
-        }),
-        body: JSON.stringify(newEntry)
+  fetch(`http://localhost:3000/journal/create`, {
+    method: "POST",
+    headers: new Headers({
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    }),
+    body: JSON.stringify(newEntry),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      displayMine();
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        displayMine()
-    })
-    .catch(err => {
-        console.error(err)
-    })
-
+    .catch((err) => {
+      console.error(err);
+    });
 }
-    
-    
-    /* *************************
-     *** UPDATE JOURNAL ***
-    ************************** */
-    function editJournal(postId) {
-     console.log('editJournal Function Called')
-    const fetch_url = `http://localhost:3000/journal/update/${postId}`;
-    const accessToken = localStorage.getItem('sessionToken')
 
-    let card = document.getElementById('postId');
-    let input = document.createElement('input');
+/* *************************
+ *** UPDATE JOURNAL ***
+ ************************** */
+function editJournal(postId) {
+  console.log("editJournal Function Called");
+  const fetch_url = `http://localhost:3000/journal/update/${postId}`;
+  const accessToken = localStorage.getItem("sessionToken");
 
-    if (card.childNodes.length < 2) {
-        card.appendChild(input);
-        input.setAttribute('type', "text");
-        input.setAttribute('id', "updatedEntry");
-        input.setAttribute('placeholder', "Edit your journal entry");
-    } else {
-        
-    }
-    }
-    
-    
-    /* *************************
-     *** DELETE JOURNAL ***
-    ************************** */
-    function deleteJournal(postId) {
-     console.log('deleteJournal Function Called')
-    }
+  let card = document.getElementById("postId");
+  let input = document.createElement("input");
+
+  if (card.childNodes.length < 2) {
+    card.appendChild(input);
+    input.setAttribute("type", "text");
+    input.setAttribute("id", "updatedEntry");
+    input.setAttribute("placeholder", "Edit your journal entry");
+  } else {
+  }
+}
+
+/* *************************
+ *** DELETE JOURNAL ***
+ ************************** */
+function deleteJournal(postId) {
+  console.log("deleteJournal Function Called");
+}
